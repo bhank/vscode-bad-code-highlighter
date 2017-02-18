3b987803e5af717849d09a3651512f01b29ab725
Adam Coyne <github@mail2.coyne.nu>
2017-02-18 16:12:30 -0600

    Make it configurable

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,40 @@
 import * as vscode from 'vscode';
+import * as _ from 'lodash';
 
-// this method is called when vs code is activated
+interface Highlight {
+	filename: string;
+	text: string;
+	color: string;
+	tooltip: string;
+}
+
+interface DecorationArray {
+	[color: string] : vscode.TextEditorDecorationType
+}
+
+let decorationTypes: DecorationArray;
+let highlights: Highlight[];
+let updateTimeout: number;
+
+function loadConfig() : void {
+	console.log('badCodeHighlighter loading config');
+	let config = vscode.workspace.getConfiguration('badCodeHighlighter');
+	updateTimeout = config.get<number>('updateTimeout', 500);
+	highlights = config.get<Highlight[]>('highlights', []);
+	if(highlights && highlights.length > 0) {
+		let allDecoratorStyles : string[] = _.uniq(highlights.map(h => h.color));
+		decorationTypes = allDecoratorStyles.reduce<DecorationArray>((accum, val) => {
+			accum[val] = vscode.window.createTextEditorDecorationType({
+				backgroundColor: val,
+			});
+			return accum;
+		},{});
+	}
+};
+
 export function activate(context: vscode.ExtensionContext) {
-
-	console.log('decorator sample is activated');
-
-	const badStuffDecorationType = vscode.window.createTextEditorDecorationType({
-		backgroundColor: 'red',
-	});
+	loadConfig();
+	vscode.workspace.onDidChangeConfiguration(loadConfig);
 
 	let activeEditor = vscode.window.activeTextEditor;
 	if (activeEditor) {
@@ -32,35 +59,41 @@ export function activate(context: vscode.ExtensionContext) {
 		if (timeout) {
 			clearTimeout(timeout);
 		}
-		timeout = setTimeout(updateDecorations, 500);
+		timeout = setTimeout(updateDecorations, updateTimeout);
 	}
 
 	function updateDecorations() {
 		if (!activeEditor) {
 			return;
 		}
-		if(!activeEditor.document.fileName.match(/\.js$/i)) {
+
+		// maybe move this check into foreach
+		let activeHighlights = highlights.filter(h => activeEditor.document.fileName.match(h.filename));
+		if(activeHighlights.length == 0) {
 			return;
 		}
-		const isTest = activeEditor.document.fileName.match(/\.spec\.js$/i);
-		const regEx = /\b(?:if\(|class=)/g;
+
 		const text = activeEditor.document.getText();
-		const badStuffDecorations: vscode.DecorationOptions[] = [];
-		let match;
-		while (match = regEx.exec(text)) {
-			const startPos = activeEditor.document.positionAt(match.index);
-			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
-			let decoration;
-			if(isTest && match[0] == "if(") {
-				decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Did you mean "it("?' };
-			} else if(match[0] == "class=") {
-				decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'If this is JSX, you mean "className="' };
-			}
-			if(decoration) {
-				badStuffDecorations.push(decoration);
+
+		let activeDecorationsByColor: { [color: string]: vscode.DecorationOptions[] } = {};
+
+		for(let highlight of activeHighlights) {
+			if(activeEditor.document.fileName.match(highlight.filename)) {
+				let regEx = new RegExp(highlight.text, 'g');
+				let match;
+				while (match = regEx.exec(text)) {
+					const startPos = activeEditor.document.positionAt(match.index);
+					const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+					let decoration: vscode.DecorationOptions = { range: new vscode.Range(startPos, endPos), hoverMessage: highlight.tooltip };
+					activeDecorationsByColor[highlight.color] = activeDecorationsByColor[highlight.color] || [];
+					activeDecorationsByColor[highlight.color].push(decoration);
+				}
 			}
 		}
-		activeEditor.setDecorations(badStuffDecorationType, badStuffDecorations);
+
+		_.forEach(activeDecorationsByColor, (decorationOptions: vscode.DecorationOptions[], color: string) => {
+			activeEditor.setDecorations(decorationTypes[color], decorationOptions);
+		});
 	}
 }
 
